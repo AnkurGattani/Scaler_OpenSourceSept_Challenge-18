85a106a61f624fab3ef19c840ca3e04aa9a5e4c1
Ankur Gattani <95461354+AnkurGattani@users.noreply.github.com>
2023-09-30 16:39:44 +0000

    Added ArithmeticOperations

---
+++ ArithmeticOperations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ArithmeticOperations
+{
+    public static void main(String[] args){
+
+        int a = 6, r = 17, sum, diff, product, quotient;
+        sum = a + r;
+        diff = r - a;
+        product = a*r;
+        quotient = r/a;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + product);
+        System.out.println("Division: " + quotient);
+
+    }
+}
